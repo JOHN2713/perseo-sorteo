8581f62a5eef5da25bc8daf63317dab58bf45e72
Johny Vilaña <johnyv1305@gmail.com>
2025-09-26 17:58:00 -0500

    feat: :sparkles: mod base concurso

--- app.py
+++ app.py
@@ -22,6 +22,9 @@ def sortear():
         "Nombre": str(ganador["Nombre"]),
     })
 
+@app.route('/health')
+def salud():
+    return 'OK', 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
